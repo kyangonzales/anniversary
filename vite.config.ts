5d3adc120d7983cd847858437a128500c503f3eb
kyangonzales <kyangonzales83@gmail.com>
2025-04-22 11:13:21 +0800

    asf

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,4 @@ export default defineConfig({
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
-    build: {
-        outDir: 'public/dist', // Specify the output directory here
-    },
 });
